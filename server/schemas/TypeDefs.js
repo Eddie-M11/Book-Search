37dd6a302fc56121ed9ddccb8c4d1d699210deb4
Eddiem-11 <emonarrez.01@outllok.com>
2022-10-10 23:52:09 -0600

    typedefs and resolvers and auth

---
+++ server/schemas/TypeDefs.js
@@ -0,0 +1,34 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+    type Book {
+        _id: ID
+        authors: [String]
+        description: !String,
+        bookId: !String,
+        image: String,
+        link: String,
+        title: !String
+    }
+    type User {
+        _id: ID
+        username: !String,
+        email: !String,
+        password: !String,
+        savedBooks: [Book]
+    }
+    type Query {
+        getSingleUser(user: !ID, username: !String): User 
+    }
+    type Mutation {
+        createUser(user: !ID, username: !String, password: !String): User
+        login(username: !String, email: !String, password: !String): User
+        saveBook(_id:String, savedBooks:[Book] ): User 
+        deleteBook(_id:String, savedBooks:[Book]): User
+    }
+    type Auth {
+    token: ID!
+    user: User
+  }
+`;
+module.exports = typeDefs;
